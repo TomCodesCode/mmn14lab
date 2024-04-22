c7863035db05d9258a4cad9311a886eaaf1fb3b2
George A <george.a13@gmail.com>
2024-04-22 15:54:20 +0300

    remove main

--- front.c
+++ front.c
@@ -488,31 +488,3 @@ AST *parseAssembley (FILE *amFile) {
     return code_head;
 }
 
-
-
-char *getcwd(char *buf, size_t size);
-
-int main(int argc, char *argv[]){ 
-    char cwd[128];
-    FILE *amFile = NULL;
-    char * filename = "test.asm";
-
-    if (argc > 1)
-        filename = argv[1];
-
-    amFile = fopen(filename, "r");
-
-    getcwd(cwd, sizeof(cwd));
-    
-    if (!amFile)
-    {
-        printf("File not found: No '%s' in [%s]\n", filename, cwd);
-        return -1;
-    }
-    parseAssembley(amFile);
-
-    dumpSymbolTbl();
-
-    fclose(amFile);
-    return 0;
-}
