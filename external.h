6c2a5974eb204d38f85551cfe072182178d81824
Tom A <arie.tom@gmail.com>
2024-04-22 16:49:22 +0300

    put all prototypes in one file

---
+++ external.h
@@ -0,0 +1,14 @@
+#include "lib.h"
+
+extern int IC;
+extern int DC;
+
+int numValidInstOperands(int inst);
+char * getInstByIdx(int idx);
+SymbolsTbl * getSymbolsTbl(void);
+int addSymbolVal(char * symbol, int symbol_type, int value);
+int getSymbolVal(const char * symbol, int * value);
+int dumpSymbolTbl(void);
+int calcOpcodePart(Opcodes * opcode, int wordtype, int num, int in_line_part);
+int opcodePerOperand(AST *ast, int num, int in_line_part, int operand_idx);
+int midPassing(AST *ast);
