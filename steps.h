967fa7355c89e448e2a5ed96af01f77ace465624
Tom A <arie.tom@gmail.com>
2024-04-25 21:18:18 +0300

    reorg, comments, finalizing

--- steps.h
+++ steps.h
@@ -2,4 +2,11 @@
 
 typedef char * t_commandLine[MAX_COMMAND_SIZE];
 
+/* Front-end pass, parsing*/
 int parseAssembley (FILE *amFile, AST ** code_ast, AST ** data_ast);
+
+/* middle step */
+int middlePass(AST * code_ast, AST * data_ast);
+
+/* backend step, final */
+int backendPass(char * filename);
